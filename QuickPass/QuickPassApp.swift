718419f575e0a366b8a5704c57e81f99f5403b65
bazimR <rishaalmuhammed@gmail.com>
2025-04-18 23:20:56 +0530

    Initial Commit

---
+++ QuickPass/QuickPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickPassApp.swift
+//  QuickPass
+//
+//  Created by Rishal Bazim on 18/04/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
